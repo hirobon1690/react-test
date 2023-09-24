7dc3d7fccc9760f623d63e22902c66d9842fe034
hirobon1690 <58695125+hirobon1690@users.noreply.github.com>
2023-09-25 01:46:30 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename='/hi_gui_app/'>
+    <BrowserRouter basename='/react-test/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
